814a1f9c78d6090f4a9e76009d446884bf483480
Tiziano Coroneo <tiziano@ticketswap.com>
2020-07-24 22:53:08 +0200

    Skipping version tests for linux

--- Tests/ApolloTests/VersionNumberTests.swift
+++ Tests/ApolloTests/VersionNumberTests.swift
@@ -1,9 +1,11 @@
 import XCTest
 @testable import Apollo
 
+#if !os(Linux)
 class VersionNumberTests: XCTestCase {
   func testVersionNumberExists() {
     // It would be the first 2 digits of version number like `0.19`.
     XCTAssertGreaterThanOrEqual(ApolloVersionNumber, 0)
   }
 }
+#endif
