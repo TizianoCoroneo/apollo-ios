1f5a9bdd8b91032cc85ceffb95ae441c7983e349
Tiziano Coroneo <tiziano@ticketswap.com>
2020-07-24 12:58:50 +0200

    replaced description with bytestring

--- Sources/Apollo/String+SHA.swift
+++ Sources/Apollo/String+SHA.swift
@@ -6,6 +6,6 @@ extension String {
   var sha256Hash: String {
     let data = self.data(using: .utf8)!
     let hash = SHA256.hash(data: data)
-    return hash.description
+    return hash.compactMap { String(format: "%02x", $0) }.joined()
   }
 }
