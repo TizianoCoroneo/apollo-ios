06d7ef5e9d4be2fc09126b08a2c3500c83b9f8b6
Tiziano Coroneo <tiziano@ticketswap.com>
2020-07-24 13:07:31 +0200

    added fallback for older macos

--- Sources/Apollo/String+SHA.swift
+++ Sources/Apollo/String+SHA.swift
@@ -3,13 +3,31 @@ import Foundation
 import Crypto
 #else
 import CryptoKit
+import CommonCrypto
 #endif
 
 extension String {
 
   var sha256Hash: String {
     let data = self.data(using: .utf8)!
+
+    #if os(Linux)
     let hash = SHA256.hash(data: data)
     return hash.compactMap { String(format: "%02x", $0) }.joined()
+
+    #else
+    if #available(OSX 10.15, *) {
+        let hash = SHA256.hash(data: data)
+        return hash.compactMap { String(format: "%02x", $0) }.joined()
+
+    } else {
+        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
+        }
+
+        return hash.compactMap { String(format: "%02x", $0) }.joined()
+    }
+    #endif
   }
 }
